80c8074470c09dabb1403c465e9aafbf205e328a
fmalcher <mail@ferdinand-malcher.de>
2014-04-23 00:07:31 +0200

    changed JSON format

--- songlist_json.php
+++ songlist_json.php
@@ -2,14 +2,12 @@
 require("mysql_connect.php");
 header("Content-type: application/json");
 ?>
-{
-"songlist" : [
+[
 <?php
 $sql = mysql_query("SELECT id, interpret, title FROM songlist ORDER BY interpret, title ASC;");
 while($row = mysql_fetch_assoc($sql)){
-	echo "{ \"id\" : \"".$row['id']."\", \"song\" : \"".$row['interpret']." - ".$row['title']."\" }\n";	
+	echo "{ \"id\" : \"".$row['id']."\", \"song\" : \"".$row['interpret']." - ".$row['title']."\" },\n";	
 }
 ?>
 
 ]
-}

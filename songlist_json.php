8e77d73bcd8e68e11b402640612252dd51f62ab7
fmalcher <mail@ferdinand-malcher.de>
2014-04-22 23:57:39 +0200

    songlist as JSON output

---
+++ songlist_json.php
@@ -0,0 +1,15 @@
+<?php
+require("mysql_connect.php");
+header("Content-type: application/json");
+?>
+{
+"songlist" : [
+<?php
+$sql = mysql_query("SELECT id, interpret, title FROM songlist ORDER BY interpret, title ASC;");
+while($row = mysql_fetch_assoc($sql)){
+	echo "{ \"id\" : \"".$row['id']."\", \"value\" : \"".$row['interpret']." - ".$row['title']."\" }\n";	
+}
+?>
+
+]
+}

37db32d5b2e1d272869eed47d9c6e4eb31d0ce7e
fmalcher <mail@ferdinand-malcher.de>
2014-04-23 14:40:49 +0200

    no comma for last entry

--- get_suggestions.php
+++ get_suggestions.php
@@ -9,15 +9,25 @@ if(!$search){
 }
 
 $sql = mysql_query("SELECT id, interpret, title FROM songlist WHERE interpret LIKE '%".$search."%' OR title LIKE '%".$search."%' LIMIT ".$limit.";");
+$num = mysql_num_rows($sql);
 
-if(mysql_num_rows($sql) < 1){
+
+if($num < 1){
 	die();
 }
 
 echo "[\n";
 
+$i = 1;
 while($row = mysql_fetch_assoc($sql)){
-	echo "{ \"id\" : \"".$row['id']."\", \"value\" : \"".$row['interpret']." - ".$row['title']."\" },";
+	echo "{ \"id\" : \"".$row['id']."\", \"value\" : \"".$row['interpret']." - ".$row['title']."\" }";
+	if($i != $num){
+		echo ",";
+	}
+	echo "\n";
+	
+	$i++;
+	
 }
 
 echo "]\n";

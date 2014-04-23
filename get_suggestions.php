9c92bad3288684886faa13ee0f0b99e6923ff0d5
fmalcher <mail@ferdinand-malcher.de>
2014-04-23 14:47:44 +0200

    SQL order and correct json

--- get_suggestions.php
+++ get_suggestions.php
@@ -4,32 +4,30 @@ require("mysql_connect.php");
 $limit = 5;
 $search = $_GET['search'];
 
-if(!$search){
-	exit();
-}
-
-$sql = mysql_query("SELECT id, interpret, title FROM songlist WHERE interpret LIKE '%".$search."%' OR title LIKE '%".$search."%' LIMIT ".$limit.";");
-$num = mysql_num_rows($sql);
 
 
-if($num < 1){
-	die();
-}
+if($search){
+	$sql = mysql_query("SELECT id, interpret, title FROM songlist WHERE interpret LIKE '%".$search."%' OR title LIKE '%".$search."%' ORDER BY interpret, title ASC LIMIT ".$limit.";");
+	$num = mysql_num_rows($sql);
 
-echo "[\n";
 
-$i = 1;
-while($row = mysql_fetch_assoc($sql)){
-	echo "{ \"id\" : \"".$row['id']."\", \"value\" : \"".$row['interpret']." - ".$row['title']."\" }";
-	if($i != $num){
-		echo ",";
+	if($num < 1){
+
+		$i = 1;
+		while($row = mysql_fetch_assoc($sql)){
+			$out .= "{ \"id\" : \"".$row['id']."\", \"value\" : \"".$row['interpret']." - ".$row['title']."\" }";
+			if($i != $num){
+				$out .= ",";
+			}
+			$out .= "\n";
+	
+			$i++;
+	
+		}
 	}
-	echo "\n";
-	
-	$i++;
-	
 }
-
+echo "[\n";
+echo $out;
 echo "]\n";
 
 

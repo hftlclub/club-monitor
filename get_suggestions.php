871fe59f4e902281a79710fd11715fbae4c96339
fmalcher <mail@ferdinand-malcher.de>
2014-04-23 14:57:18 +0200

    multiple search words

--- get_suggestions.php
+++ get_suggestions.php
@@ -2,19 +2,35 @@
 require("mysql_connect.php");
 
 $limit = 5;
-$search = $_GET['search'];
+$search = explode(" ", $_GET['search']);
+
+
 
 
 
 if($search){
-	$sql = mysql_query("SELECT id, interpret, title FROM songlist WHERE interpret LIKE '%".$search."%' OR title LIKE '%".$search."%' ORDER BY interpret, title ASC LIMIT ".$limit.";");
-	$num = mysql_num_rows($sql);
+	$sql = "SELECT id, interpret, title FROM songlist WHERE ";
+	for($i = 0; $i < count($search); $i++){
+		$sql .= "interpret LIKE '%".$search[$i]."%' OR title LIKE '%".$search[$i]."%'";
+		
+		if($i < (count($search) - 1)){
+			$sql .= " OR ";
+		}
+	}
+	
+	
+	$sql .= " ORDER BY interpret, title ASC LIMIT ".$limit.";";
+	
+	die($sql);
+	
+	$query = mysql_query($sql);
+	$num = mysql_num_rows($query);
 
 
 	if($num > 0){
 
 		$i = 1;
-		while($row = mysql_fetch_assoc($sql)){
+		while($row = mysql_fetch_assoc($query)){
 			$out .= "{ \"id\" : \"".$row['id']."\", \"value\" : \"".$row['interpret']." - ".$row['title']."\" }";
 			if($i != $num){
 				$out .= ",";

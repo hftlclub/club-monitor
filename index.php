b3ddc39b180233a4163e0d8a4d7756348eb76a1a
Tilmann Bach <tilli14@msn.com>
2014-04-26 23:52:15 +0200

    added modul chooser

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HfTL-Club Infoscreen</title>
+
+    <!-- Bootstrap -->
+    <link href="../common/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../common/css/darktheme.css" rel="stylesheet">
+    <link href="../common/css/style.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div>
+                <h1>HfTL-Club Infoscreen</h1>
+            </div>
+        </div>
+        <div class="row">
+             <div>
+                 <a href="./pizza/">
+                     <div class="center-block" style="text-align:center;">
+                         <button type="button" class="btn btn-default btn-lg">
+                             <span class="glyphicon glyphicon-shopping-cart largeicon"></span>
+                         </button>
+                         <br />
+                         <h2>Pizza Modul</h2>
+                     </div>
+                 </a>
+                 <a href="./karaoke/">
+                     <div class="center-block" style="text-align:center;">
+                         <button type="button" class="btn btn-default btn-lg">
+                             <span class="glyphicon glyphicon-music largeicon"></span>
+                         </button>
+                         <br />
+                         <h2>Karaoke Modul</h2>
+                     </div>
+                 </a>
+                 <a href="./slideshow/">
+                     <div class="center-block" style="text-align:center;">
+                         <button type="button" class="btn btn-default btn-lg">
+                             <span class="glyphicon glyphicon-picture largeicon"></span>
+                         </button>
+                         <br />
+                         <h2>Slideshow Modul</h2>
+                     </div>
+                 </a>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../common/js/bootstrap.min.js"></script>
+</body>
+</html>

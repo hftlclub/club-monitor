d5eda537e928d9e266c15b792cffcdbd932a911a
Tilmann Bach <tilli14@msn.com>
2014-04-26 23:38:11 +0200

    added bootstrap theme to karaoke manager backend

--- karaoke/manager/index.php
+++ karaoke/manager/index.php
@@ -9,7 +9,7 @@ $id   = $_GET['id'];
 if($mode == "unregister" && !empty($id)){
 	$sql = "UPDATE queue SET played = 1 WHERE id = '".$id."';";
 	mysql_query($sql);
-	
+
 	header("Location: index.php");
 	exit();
 }
@@ -17,37 +17,77 @@ if($mode == "unregister" && !empty($id)){
 
 //GET without parameters
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="de">
 <head>
-<title>Karaoke Manager</title>
-<meta http-equiv="refresh" content="3; URL=<?php echo $_SERVER['REQUEST_URI'] ?>">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="refresh" content="3; URL=<?php echo $_SERVER['REQUEST_URI'] ?>">
+    <title>Karaokemanager - Management</title>
+
+    <!-- Bootstrap -->
+    <link href="../common/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../common/css/darktheme.css" rel="stylesheet">
+    <link href="../common/css/style.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
+    <div class="container-fluid">
+        <div class="row">
+            <div>
+                <h1>Karaoke Manager</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div>
+                <table class="table table-striped">
+                    <tr>
+                        <th>#</th>
+                        <th>Interpret</th>
+                        <th>Titel</th>
+                        <th>eingetragen von</th>
+                        <th>&nbsp;</th>
+                    </tr>
 
-<h1>Karaoke Manager</h1>
+                    <?php
 
-<table cellpadding="5" border="1">
-	<tr>
-		<th>#</th>
-		<th>Interpret</th>
-		<th>Titel</th>
-		<th>eingetragen von</th>
-		<th>&nbsp;</th>
-	</tr>
+                    $sql = mysql_query("SELECT songlist.interpret, songlist.title, queue.singer, queue.id FROM songlist, queue WHERE queue.songid = songlist.id AND queue.played = 0 ORDER BY timestamp ASC;");
+                    $i = 1;
+                    while($row = mysql_fetch_assoc($sql)){
+                    echo "
+                    <tr>
+                        \n";
+                        echo "
+                        <td>".$i++."</td>\n";
+                        echo "
+                        <td>".$row['interpret']."</td>\n";
+                        echo "
+                        <td>".$row['title']."</td>\n";
+                        echo "
+                        <td>".$row['singer']."</td>\n";
+                        echo "
+                        <td><a href=\"index.php?mode =unregister&id =" . $row['id'] . " \">Entfernen</a></td>\n";
+                        echo "
+                    </tr>\n";
+                    }
 
-<?php
+                    ?>
+                </table>
+            </div>
+        </div>
+    </div>
 
-$sql = mysql_query("SELECT songlist.interpret, songlist.title, queue.singer, queue.id FROM songlist, queue WHERE queue.songid = songlist.id AND queue.played = 0 ORDER BY timestamp ASC;");
-$i = 1;
-while($row = mysql_fetch_assoc($sql)){
-	echo "	<tr>\n";
-	echo "		<td>".$i++."</td>\n";
-	echo "		<td>".$row['interpret']."</td>\n";
-	echo "		<td>".$row['title']."</td>\n";
-	echo "		<td>".$row['singer']."</td>\n";
-	echo "		<td><a href=\"index.php?mode=unregister&id=".$row['id']."\">Entfernen</a></td>\n";
-	echo "	</tr>\n";
-}
 
-?>
-</table>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../common/js/bootstrap.min.js"></script>
+</body>
+</html>

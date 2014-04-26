f9cd14327b32269918bff6d0f60481efa22849fc
fmalcher (srv2) <mail@ferdinand-malcher.de>
2014-04-27 01:18:34 +0200

    custom function myuniqid() in config.php

--- common/config.example.php
+++ common/config.example.php
@@ -1,5 +1,5 @@
 <?php
-### RENAME THIS FILE TO config.php ###
+### COPY THIS FILE TO config.php ###
 
 
 //MySQL settings
@@ -15,6 +15,16 @@ define("DATABASE_NAME",       "club");
 
 //connect to MySQLServer
 $dbc = mysql_connect(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD);
+mysql_set_charset("utf8", $dbc);
 mysql_select_db(DATABASE_NAME, $dbc);
+
+
+
+
+//functions - should be moved to separate file
+function myuniqid(){
+	return substr(md5(uniqid(rand(0,time()),1)),0,13);
+}
+
 
 ?>

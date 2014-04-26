6d63eeecc1283feec4aff6412083c95973e4e07b
Tilmann Bach <tilli14@msn.com>
2014-04-27 00:38:23 +0200

    debugged paths -.-

--- karaoke/index.php
+++ karaoke/index.php
@@ -9,7 +9,7 @@
 
     <!-- Bootstrap -->
     <link href="../common/css/bootstrap.min.css" rel="stylesheet">
-    <link href="./common/css/bootstrap.cyborg.min.css" rel="stylesheet">
+    <link href="../common/css/bootstrap.cyborg.min.css" rel="stylesheet">
     <link href="../common/css/style.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->

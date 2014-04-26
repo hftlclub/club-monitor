920585993579f51e0145cd371587dc644e1ef6cf
Dörki <Danny.Koppenhagen@t-online.de>
2014-04-26 22:57:33 +0200

    bug fix: path changes ; changed language

--- karaoke/index.php
+++ karaoke/index.php
@@ -1,5 +1,5 @@
 ﻿<!DOCTYPE html>
-<html lang="en">
+<html lang="de">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -68,6 +68,6 @@ while($row = mysql_fetch_assoc($sql)):
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="../common/js/bootstrap.min.js"></script>
   </body>
 </html>

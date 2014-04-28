a67da372d56e3323c8be07697edd0b366ce26726
fmalcher (srv2) <mail@ferdinand-malcher.de>
2014-04-28 16:12:19 +0200

    unpay function, pay on button click, delete function (not finished)

--- manager/pizza/db_actions.php
+++ manager/pizza/db_actions.php
@@ -9,6 +9,8 @@ if($_SERVER['REQUEST_METHOD'] != "POST"){
 
 $data = json_decode(file_get_contents("php://input"), true);
 
+//error_log(print_r($data, true)); exit();
+
 
 $data['mode'] = mysql_real_escape_string($data['mode']);
 $data['id']   = mysql_real_escape_string($data['id']);
@@ -19,7 +21,7 @@ if(!$data['mode']){
 }
 
 
-if($data['mode'] == "pay" && $data['id']){
+if(($data['mode'] == "pay" OR $data['mode'] == "unpay") AND $data['id']){
 	//check whether order ID exists
 	$query = "SELECT id FROM pizza_orders WHERE id = '".$data['id']."';";
 	$sql = mysql_query($query);
@@ -29,7 +31,29 @@ if($data['mode'] == "pay" && $data['id']){
 		doerror();
 	}
 	
-	$query = "UPDATE pizza_orders SET paid = 1 WHERE id = '".$data['id']."' LIMIT 1;";
+	//pay or unpay?
+	$newpaid = ($data['mode'] == "pay") ? 1 : 0;
+		
+	$query = "UPDATE pizza_orders SET paid = ".$newpaid." WHERE id = '".$data['id']."' LIMIT 1;";
+	if(!mysql_query($query)){
+		doerror();
+	}
+}
+
+
+
+if($data['mode'] == "deleteOrder" AND $data['id']){
+	//check whether order ID exists
+	$query = "SELECT id FROM pizza_orders WHERE id = '".$data['id']."';";
+	$sql = mysql_query($query);
+	
+	//error if ID is not valid
+	if(mysql_num_rows($sql) != 1){
+		doerror();
+	}
+	
+	
+	$query = "DELETE FROM pizza_orders WHERE id = '".$data['id']."' LIMIT 1;";
 	if(!mysql_query($query)){
 		doerror();
 	}

fd280e0dbd99792c1cd554b7434c26d2093c7e84
Tilmann Bach <tilli14@msn.com>
2014-08-05 20:45:26 +0200

    formatted js file

--- manager/infoscreen/manager.js
+++ manager/infoscreen/manager.js
@@ -16,87 +16,87 @@
 angular.module('steckerApp', ['ui.sortable'])
 
 
-.controller('InfoscreenMngrCtrl', function($scope) {
+.controller('InfoscreenMngrCtrl', function ($scope) {
 })
 
 
-.controller('InfoscreenMngrCtrl', function($scope, $http, $q) {
+.controller('InfoscreenMngrCtrl', function ($scope, $http, $q) {
 	$scope.active = { id: 0 };
 	$scope.hasTouch = isTouchSupported();
 
 	$scope.sortableOptions = {
-		stop: function(e, ui) {
+		stop: function (e, ui) {
 			$scope.submitOrder();
 		},
 		handle: '.module-order-handle',
 		axis: 'y',
 	};
 
-	$scope.remove = function(index) {
+	$scope.remove = function (index) {
 		$scope.timeline.splice(index, 1);
 
-};
+	};
 
-$scope.moveDown = function(index) {
-$scope.swap(index, index+1);
-};
+	$scope.moveDown = function (index) {
+		$scope.swap(index, index + 1);
+	};
 
-$scope.moveUp = function(index) {
-$scope.swap(index, index-1);
-};
+	$scope.moveUp = function (index) {
+		$scope.swap(index, index - 1);
+	};
 
-$scope.swap = function(a, b) {
-var l = $scope.timeline;
-l[a] = l.splice(b, 1, l[a])[0];
-$scope.submitOrder();
-};
+	$scope.swap = function (a, b) {
+		var l = $scope.timeline;
+		l[a] = l.splice(b, 1, l[a])[0];
+		$scope.submitOrder();
+	};
 
-$scope.add = function(type) {
-$http.post('json.php?mode=addItem', {
-  type: type,
-  order: $scope.timeline.length
-}).success(function(data){
-  $scope.refresh().then(function(data){
-  });
-});
-};
+	$scope.add = function (type) {
+		$http.post('json.php?mode=addItem', {
+			type: type,
+			order: $scope.timeline.length
+		}).success(function (data) {
+			$scope.refresh().then(function (data) {
+			});
+		});
+	};
 
-$scope.submitOrder = function() {
-var dout = [];
+	$scope.submitOrder = function () {
+		var dout = [];
 
-$scope.timeline.forEach(function(obj, i){
-  dout.push({order: i, id: obj.id});
-});
+		$scope.timeline.forEach(function (obj, i) {
+			dout.push({ order: i, id: obj.id });
+		});
 
-$http.post('json.php?mode=reorderTimeline', dout).success($scope.refresh);
+		$http.post('json.php?mode=reorderTimeline', dout).success($scope.refresh);
 
-};
+	};
 
-$scope.disable = function(id) {
-$http.post('json.php?mode=disableItem', [{id:id}]).success($scope.refresh);
-};
+	$scope.disable = function (id) {
+		$http.post('json.php?mode=disableItem', [{ id: id }]).success($scope.refresh);
+	};
 
-$scope.activate = function(id) {
-$http.post('json.php?mode=activateItem', [{id:id}]).success($scope.refresh);
-};
+	$scope.activate = function (id) {
+		$http.post('json.php?mode=activateItem', [{ id: id }]).success($scope.refresh);
+	};
 
-$scope.remove = function(id) {
-$http.post('json.php?mode=deleteItem', [{id:id}]).success($scope.refresh);
-};
+	$scope.remove = function (id) {
+		$http.post('json.php?mode=deleteItem', [{ id: id }]).success($scope.refresh);
+	};
 
-$scope.refresh = function() {
-var deferred = $q.defer();
+	$scope.refresh = function () {
+		var deferred = $q.defer();
 
-$http.get('json.php?mode=retrieve').success(function(data){
-  $scope.timeline = data;
-  deferred.resolve(data);
-});
+		$http.get('json.php?mode=retrieve').success(function (data) {
+			$scope.timeline = data;
+			deferred.resolve(data);
+		});
 
-return deferred.promise;
+		return deferred.promise;
 
-};
+	};
 
-$scope.refresh();
+	$scope.refresh();
 
 
 });

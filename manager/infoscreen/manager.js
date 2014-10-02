ad71744742f8e15f4ca9337099c236fe188d0a3b
fmalcher (srv2) <mail@ferdinand-malcher.de>
2014-10-02 23:43:37 +0200

    subnav active state, powered by @svenwltr

--- manager/infoscreen/manager.js
+++ manager/infoscreen/manager.js
@@ -199,4 +199,43 @@ angular.module('steckerApp', ['ui.sortable', 'ngRoute', 'angularFileUpload'])
 	$scope.refresh();
 
 
+})
+
+
+.directive('subnavActiveState', function($location) {
+	return {
+		restrict: 'AC',
+		link: function(scope, element, attr) {
+			var a = element.find('a');
+
+			/* refresh, when location changed */
+			scope.$on('$locationChangeSuccess', refresh);
+
+			/* refresh, when href changed (ie by using ng-href) */
+			scope.$watch( /* may impact performance */
+				function(){ return a.attr('href') },
+				function(){ refresh(); }
+			);
+
+			/* refresh on load */
+			refresh();
+
+			function refresh() {
+				var href = a.attr('href');
+				var curr = '#' + $location.path();
+
+				if(curr.indexOf(href) == 0)
+					attr.$addClass('active');
+				else
+					attr.$removeClass('active');
+
+			}
+
+		},
+
+	}
+
 });
+
+
+

05b41ca4d09a52eafc27c188bdf33eecc77dd6b8
fmalcher (srv2) <mail@ferdinand-malcher.de>
2014-11-18 14:45:33 +0100

    Import script TXT to SQL

---
+++ karaoke/scripts/sql_importtxt.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Import karaoke songlist from exported txt file
+Ferdinand Malcher 2014
+*/
+
+require("../../common/config.php");
+
+//truncate database
+mysql_query("TRUNCATE songlist;");
+
+
+//read file to array
+$file = file("songliste.txt");
+
+foreach($file AS $line){
+	//make array from line
+	$row = explode("|", $line);
+	
+	//trim and escape all values
+	array_walk($row, create_function('&$val', '$val = mytrim($val);'));
+	array_walk($row, create_function('&$val', '$val = mysql_escape_string($val);'));
+	$row[0] = intval($row[0]);
+	
+	//skip if no ID is given
+	if(!$row[0]){
+		continue;
+	}
+	
+	//insert row into DB
+	$query = "INSERT INTO songlist(id, interpret, title, language) VALUES ('".$row[0]."', '".$row[1]."', '".$row[2]."', '".$row[3]."');";
+	mysql_query($query);
+	
+	
+}
+
+
+
+function mytrim($string){
+	return preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $string);
+}
+
+?>
